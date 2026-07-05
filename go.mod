000376ab6b61f02639affc1342fc9001b5cd69bd
Joao-POP <joaopap@proton.me>
2026-07-05 11:36:41 -0300

    Created profcircuit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module profcircuit
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,8 @@
+// Copyright 2026 João Pedro Alves Pinto <joaopap@proton.me>
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi, Azrael!")
+}
