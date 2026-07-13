e2234e263cc867f0c4dfc14d8a0cc48614e9fa9a
Joao-POP <joaopap@proton.me>
2026-07-13 13:16:57 -0300

    Switching away from Go for this project. Seems Wasm support is flaky and it doesn't make sense to put a GC in the middle of this anyways.

--- go.mod
+++
@@ -1,3 +0,0 @@
-module profcircuit
-
-go 1.25.0
--- main.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2026 João Pedro Alves Pinto <joaopap@proton.me>
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("Hi, Azrael!")
-}
