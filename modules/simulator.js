70a598e8e824270a10bbded63e9aff71e5b09ed9
Joao-POP <joaopap@proton.me>
2026-07-14 15:22:36 -0300

    Added algorithm for resistance calculation.

--- modules/simulator.js
+++ modules/simulator.js
@@ -1,7 +1,37 @@
-export { getEquivalentResistance };
+export { getEquivalentResistance, Node };
 
-function getEquivalentResistance(circuit) {
-    let equivalentResistance = 0;
+function Node(parts, connections) {
+    this.parts = parts;
+    this.connections = connections;
+}
+
+function getEquivalentResistance(circuitNode) {
+    if (circuitNode.parts === null) {
+        return 0;
+    }
+
+    if (circuitNode.parts.length != circuitNode.connections.length) {
+        throw "Unequal number of connections and parts";
+    }
+
+    let branchResistances = [];
+
+    // Make every out node into an equivalent resistance.
+    for (let i = 0; i < circuitNode.parts.length; i++) {
+        let branchEquivalentResistance = 0;
+
+        branchEquivalentResistance += circuitNode.parts[i] +
+            getEquivalentResistance(circuitNode.connections[i]);
+
+        branchResistances.push(branchEquivalentResistance);
+    }
+
+    let equivalentResistance = getParallelResistance(branchResistances);
 
     return equivalentResistance;
+}
+
+function getParallelResistance(resistors) {
+    console.log(resistors);
+    return Math.sumPrecise(resistors.map((r) => r ** -1)) ** -1;
 }
