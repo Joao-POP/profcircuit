b526213183b56242521b4094e5698c1b0572f029
João Pedro <joaopap@proton.me>
2026-07-15 20:45:28 -0300

    This is my resignation letter from trying to make a sensible equivalent resistance function

--- modules/simulator.js
+++ modules/simulator.js
@@ -1,62 +1,7 @@
-export { getEquivalentResistance, Node };
+export { start };
 
-class Node {
-    constructor(parts, connections) {
-        this.parts = parts;
-        this.connections = connections;
-        this.symMap = new Map();
-    }
-}
+function start();
 
-// Mark and sweep, kinda.
-function getEquivalentResistance(circuitNode) {
-    console.debug("Entering node", circuitNode.tag, circuitNode);
-
-    if (circuitNode.parts === null) {
-        console.debug("Circuit termination");
-        return 0;
-    }
-
-    if (circuitNode.parts.length != circuitNode.connections.length) {
-        throw "Unequal number of connections and parts";
-    }
-
-    // Keep running while termination is not reached.
-    //while (circuitNode.parts.length > 1 && circuitNode.connections.length > 1 && circuitNode.connections[0].parts != null) {
-        markNodes(circuitNode);
-        sweepNodes(circuitNode);
-    //}
-
-    // Last resistor standing is the equivalent one.
-    return circuitNode.parts[0][0];
-}
-
-// Traces where current came from.
-function markNodes(node) {
-    for (let c of node.connections) {
-        if (node.connections.length > 1) {
-            c.symMap.set(node, (c.symMap.get(node) ?? 0) + 1);
-        }
-
-        node.symMap.entries().forEach(sym => {
-            c.symMap.set(sym, (c.symMap.get(sym) ?? 0) + 1);
-        });
-
-        markNodes(c);
-    }
-}
-
-// Merges nodes as to join their split currents back.
-function sweepNodes(terminatorNode) {
-
-}
-
-function getParallelResistance(resistors) {
-    console.debug("Parallel of:", resistors);
-
-    let result = Math.sumPrecise(resistors.map((r) => r ** -1)) ** -1;
-
-    console.debug("Got", result);
-
-    return result;
+function calculateParallelResistance(resistors) {
+    return Math.sumPrecise(resistors.map((r) => r ** -1)) ** -1;
 }
