25e6c02acea4cd1e16fd5462362470bf5a3dfe86
Joao-POP <joaopap@proton.me>
2026-07-23 19:45:55 -0300

    Added a link to the source and a dropdown menu

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 import * as Sim from "./modules/simulator.js";
 
+const addMenuItem = document.getElementById("add-menu-item");
+const addDropdown = document.getElementById("add-dropdown");
+
+addMenuItem.onclick = () => {
+    addMenuItem.classList.toggle("active");
+    addDropdown.classList.toggle("active");
+
+    addDropdown.style.display = addDropdown.style.display === "block" ? "none" : "block";
+};
+
 Sim.start();
